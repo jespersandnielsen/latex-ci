eef50a4d8e15789bb0987713166b2190c97a8cae
Emil Bækdahl <emilsbaekdahl@gmail.com>
2017-10-11 09:47:09 +0200

    Add Octokit for Github API integration

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'sinatra', '~> 2.0'
 gem 'json', '~> 2.1'
+gem "octokit", '~> 4.0'
--- latex_ci.rb
+++ latex_ci.rb
@@ -6,5 +6,11 @@ get '/' do
 end
 
 post '/build' do
-  
+  payload = JSON.parse(params[:payload])
+  event_type = request.env['HTTP_X_GITHUB_EVENT']
+
+  case event_type
+  when 'push'
+    payload
+  end
 end
