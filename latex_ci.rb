a91a85b188a54b028e9f2260528082667c3f9eba
Emil Bækdahl <emilsbaekdahl@gmail.com>
2017-10-11 11:45:06 +0200

    Abstract pull/clone of repo into  pull_repo function

--- latex_ci.rb
+++ latex_ci.rb
@@ -2,8 +2,6 @@ require 'sinatra'
 require 'json'
 require 'git'
 
-repo = ''
-
 get '/' do
   'Latex-CI'
 end
@@ -14,16 +12,23 @@ post '/build' do
 
   case event_type
   when 'push'
-    if File.directory?('project')
-      g = Git.open('project')
-      g.pull
+    pull_repo(payload['repository'])
+  end
+end
 
-      Dir.chdir 'project'
-      system("latexmk -c")
-      system("latexmk > log.txt")
-    else
-      g = Git.clone(repo, 'project')
-    end
+def pull_repo(repo)
+  repo_name = "builds/#{repo['name']}"
+  repo_url = repo['url']
+
+  if File.directory? repo_name
+    g = Git.open repo_name
+    g.pull
+
+    Dir.chdir repo_name
+    system 'latexmk -c'
+    system "latexmk > log.txt"
+  else
+    g = Git.clone repo_url, repo_name
   end
 end
 
