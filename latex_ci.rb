ffd79ab8f9b19822d284d960ef4796426c984f2c
Emil Bækdahl <emilsbaekdahl@gmail.com>
2017-10-11 12:19:10 +0200

    Move building to separate function

--- latex_ci.rb
+++ latex_ci.rb
@@ -7,31 +7,33 @@ get '/' do
 end
 
 post '/build' do
-  payload = JSON.parse(params[:payload])
+  payload = JSON.parse params[:payload]
+  repo = payload['repository']
   event_type = request.env['HTTP_X_GITHUB_EVENT']
 
   case event_type
   when 'push'
-    pull_repo(payload['repository'])
+    pull_repo repo
+    build_repo repo
   end
 end
 
 def pull_repo(repo)
-  repo_name = "builds/#{repo['name']}"
+  repo_dir = "builds/#{repo['name']}"
   repo_url = repo['url']
 
-  if File.directory? repo_name
-    g = Git.open repo_name
+  if File.directory? repo_dir
+    g = Git.open repo_dir
     g.pull
-
-    Dir.chdir repo_name
-    system 'latexmk -c'
-    system "latexmk > log.txt"
   else
-    g = Git.clone repo_url, repo_name
+    g = Git.clone repo_url, repo_dir
   end
 end
 
-get '/log' do
-  File.read(File.join(File.dirname(__FILE__), 'project/log.txt'))
+def build_repo(repo)
+  repo_dir = "builds/#{repo['name']}"
+
+  Dir.chdir repo_dir
+  system 'latexmk -c'
+  system 'latexmk > log.txt'
 end
