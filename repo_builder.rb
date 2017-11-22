e084109cde0cfa89bc2acb36923c2f5c05a8899b
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2017-11-22 20:16:49 +0100

    fix build server

--- repo_builder.rb
+++ repo_builder.rb
@@ -20,9 +20,6 @@ class RepoBuilder
     when 'push'
       pull_repo
       exitcode = build_repo
-
-      p "Test"
-      p exitcode
     end
 
     status = case exitcode
@@ -59,10 +56,10 @@ private
     # exitcode = CommandRunner.run "dotnet test test/Petra.Model.Test/Petra.Model.Test.csproj", @repo_dir if exitcode == 0
 
     # Code coverage
-    exitcode = CommandRunner.run "./build.ps1 --codecoverage" unless exitcode == 0
+    exitcode = CommandRunner.run "./build.ps1 --codecoverage", @repo_dir unless exitcode == 0
 
     # publish coverage result
-    exitcode = CommandRunner.run "./build.ps1 --publish --branch #{@branch}" unless exitcode == 0
+    exitcode = CommandRunner.run "./build.ps1 --publish --branch #{@branch}", @repo_dir unless exitcode == 0
 
     exitcode
   end
