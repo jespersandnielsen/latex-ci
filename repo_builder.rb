74488835ce524b156e115d09d29635fd15f42bf8
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2017-11-23 12:18:47 +0100

    update argumetns to build script

--- repo_builder.rb
+++ repo_builder.rb
@@ -58,10 +58,10 @@ private
     # exitcode = CommandRunner.run "dotnet test test/Petra.Model.Test/Petra.Model.Test.csproj", @dir if exitcode == 0
 
     # Code coverage
-    exitcode = CommandRunner.run "./build.ps1 --codecoverage", @dir unless exitcode == 0
+    exitcode = CommandRunner.run "./build.ps1 -codecoverage", @dir unless exitcode == 0
 
     # publish coverage result
-    exitcode = CommandRunner.run "./build.ps1 --publish --branch #{@branch}", @dir unless exitcode == 0
+    exitcode = CommandRunner.run "./build.ps1 -publish -branch #{@branch}", @dir unless exitcode == 0
 
     exitcode
   end
